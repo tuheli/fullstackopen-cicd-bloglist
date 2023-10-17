cc0edddbb0e46653b6245f80f4cd98f74b322bac
tuheli <elias.tuhola@gmail.com>
2023-10-17 16:00:12 +0300

    Revert "Deployment workflow: testing backend lint error"
    This reverts commit a4eb214485754d4b21c74eaf749f06adf87db114.

--- bloglist-backend/index.js
+++ bloglist-backend/index.js
@@ -1,6 +1,6 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const app = require("./app")
+const app = require('./app')
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}.`)

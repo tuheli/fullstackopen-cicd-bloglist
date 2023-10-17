db914c54aece0682fac553cb114ccc91d135cc89
tuheli <elias.tuhola@gmail.com>
2023-10-17 15:40:15 +0300

    Revert "Deployment workflow: testing eslint error"
    This reverts commit ede5e244075166ddb2e312e4d01833fe54fb31e8.

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 const Blog = ({ blog, user, handleLikeClick, handleDeleteClick }) => {
   const [showFullInfo, setShowFullInfo] = useState(false)

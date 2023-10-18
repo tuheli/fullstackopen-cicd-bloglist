d1ed8858e19cda6b9a4c049faedbe37fb40a5c9a
tuheli <elias.tuhola@gmail.com>
2023-10-18 23:39:51 +0300

    Trigger workflow push

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const app = require('./app')
 app.listen(config.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${config.PORT}.`)
+
+  // eslint-disable-next-line no-console
+  console.log('trigger workflow push')
 })

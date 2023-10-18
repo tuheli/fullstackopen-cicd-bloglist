3a008055453f7af2360cad67bb3f90571ffe6c50
tuheli <elias.tuhola@gmail.com>
2023-10-18 13:56:23 +0300

    Update index.js

--- bloglist-backend/index.js
+++ bloglist-backend/index.js
@@ -1,7 +1,7 @@
 const config = require('./utils/config')
-const logger = require('./utils/logger')
 const app = require('./app')
 
 app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}.`)
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${config.PORT}.`)
 })

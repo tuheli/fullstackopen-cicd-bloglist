308ecbe532a90156f90ffd1e974338bc0b90a24f
tuheli <elias.tuhola@gmail.com>
2023-10-19 16:32:42 +0300

    Removed unnecessary log, #SKIP

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ const app = require('./app')
 app.listen(config.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${config.PORT}.`)
-  // eslint-disable-next-line no-console
-  console.log('testing versioning')
 })

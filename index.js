1c3c2bfbb6bdfa66c8ae84e5def36b27e264beda
tuheli <elias.tuhola@gmail.com>
2023-10-19 16:45:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const app = require('./app')
 app.listen(config.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${config.PORT}.`)
+  // eslint-disable-next-line no-console
+  console.log('pull request for fullstackopen exercise 11.21')
 })
